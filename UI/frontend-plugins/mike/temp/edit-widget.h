a8312cc1be7474df315721df1ed9e628a5b37ad0
Ford Smith <fms34@case.edu>
2021-03-07 14:05:39 -0500

    Deleted uncessary files

--- UI/frontend-plugins/mike/temp/edit-widget.h
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-class EditOutputWidget : virtual public QDialog {
-    public:
-    virtual ~EditOutputWidget() {}
-    virtual QJsonObject Config() = 0;
-};
-
-EditOutputWidget *createEditOutputWidget(QJsonObject conf, QWidget *parent = 0);
--- UI/frontend-plugins/mike/temp/mike-api.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "curl/curl.h"
-#include <json11.hpp>
-
-std::string Login(std::string un, std::string pw);
-
-bool LoadLoginResult(std::string loginResult, json11::Json &output);
